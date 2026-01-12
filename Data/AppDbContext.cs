6cb948ccda6d1032132390aee928665ae882a9ce
Jasenovec <rg_montano@hotmail.com>
2026-01-12 16:50:07 -0500

    feat(backend): configuro DbContext en memoria y siembro datos iniciales

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CompartamosAppBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompartamosAppBackend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>().HasData(
+            new Account { Id = 1, AccountNumber = "ACC-0001", Balance = 1000m },
+            new Account { Id = 2, AccountNumber = "ACC-0002", Balance = 250m }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
